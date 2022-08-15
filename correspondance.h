28a44cc9a27701af1b93b582505849d706484227
JeremyRieussec <jeremy.rieussec@gmail.com>
2022-08-15 11:18:35 -0400

    upload files

---
+++ correspondance.h
@@ -0,0 +1,14 @@
+#ifndef _correspondance_h_
+#define _correspondance_h_
+
+
+static const int hello_user = 1;
+static const int goodbye_user = 2;
+static const int recording = 3;
+static const int stop_recording = 4;
+
+static const int print_center = 12;
+static const int print_back = 13;
+
+
+#endif // _adalogo_h_
